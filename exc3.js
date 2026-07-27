aac221d7e2f4faebf21cb5ca396820adae084471
sayayin666 <sayayinjohan4@gmail.com>
2026-07-27 13:25:06 -0500

    ejercicio3 completado

---
+++ exc3.js
@@ -0,0 +1,6 @@
+let parametro_base = prompt("escriba valor base: ");
+let parametro_altura = prompt("escriba valor altura: ");
+
+let area_triangulo = parametro_base * parametro_altura / 2;
+
+alert("el area del triagulo es de: " + area_triangulo + " mm/mts");
