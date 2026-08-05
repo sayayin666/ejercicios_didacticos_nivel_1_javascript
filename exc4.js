c45fce0960d41ea6163a9d72c13947a3ce19db77
sayayin666 <sayayinjohan4@gmail.com>
2026-08-04 22:06:52 -0500

    ejercicio 4 completado

---
+++ exc4.js
@@ -0,0 +1,9 @@
+let variable_edad = prompt("ingrese su edad en numeros: ");
+
+const año1 = 365;
+const año2 = 12;
+
+let calcular1 = variable_edad * año1;
+let calcular2 = variable_edad * año2;
+
+alert("tienes " + calcular1 + " dias, y " + calcular2 + " meses de existencia");
