5776c4349ed43c30036a77812f4c8c716b8c2e8b
sayayin666 <sayayinjohan4@gmail.com>
2026-09-05 21:48:49 -0500

    ejercicio 1 (bloque 2) - COMPLETADO

---
+++ exc11.js
@@ -0,0 +1,9 @@
+function sumatoria() {
+    let numeros = [1,2,3,4,5,6,7,8,9];
+    let contador = 0;
+    for (let i = 0; i < numeros.length; i = i + 1) {
+        console.log(contador = contador + numeros[i]);
+    }
+    
+}
+sumatoria()
