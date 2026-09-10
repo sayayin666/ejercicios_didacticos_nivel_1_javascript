9803023b9bbbc7fb5fc2655ad8e4801d96142255
sayayin666 <sayayinjohan4@gmail.com>
2026-09-09 21:40:12 -0500

    se agregó nueva's rama's: nivel_2 & nivel_3    ______________ ,,,, se eliminarón los anteriores 10 archivos .html y .js   ____________   & se añadioó y completó el nuevo archivo: exc11.js

--- exc4.js
+++
@@ -1,9 +0,0 @@
-let variable_edad = prompt("ingrese su edad en numeros: ");
-
-const año1 = 365;
-const año2 = 12;
-
-let calcular1 = variable_edad * año1;
-let calcular2 = variable_edad * año2;
-
-alert("tienes " + calcular1 + " dias, y " + calcular2 + " meses de existencia");
